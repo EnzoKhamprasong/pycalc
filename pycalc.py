592dd538e9bc99cd58dd42b0ebe2c9087c366a28
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-02-26 11:30:51 +0100

    Ajout de l'addition

--- pycalc.py
+++ pycalc.py
@@ -4,4 +4,5 @@ nb1 = float(sys.argv[1])
 nb2 = float(sys.argv[3])
 operateur = sys.argv[2]
 
-print(f"Le nombre 1 est {nb1} à qui on va faire {operateur} sur {nb2}")
+if operateur == "+":
+    print(nb1+nb2)
